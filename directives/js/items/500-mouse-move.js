75a5e7595a58158162faec2c0c50a44bee24329c
Your Name <you@example.com>
2025-03-04 23:00:21 +0100

    New File

---
+++ directives/js/items/500-mouse-move.js
@@ -0,0 +1,26 @@
+directives.OnReady(() =>
+{
+    directives.ItemAdd({
+        id: 'dh-mouse-move',
+        attribute: 'dh-mouse-move',
+        order: 500,
+        code: function(context, compile, node, identifier)
+        {
+            const attribute = node.getAttribute('dh-mouse-move');
+
+            node.removeAttribute('dh-mouse-move');
+
+            const mouseEnterHandler = (event) =>
+            {
+                const results = divhunt.Function(attribute, context);
+
+                if(typeof results === 'function')
+                {
+                    results(event, compile);
+                }
+            };
+
+            node.addEventListener('mousemove', mouseEnterHandler);
+        }
+    });
+});
