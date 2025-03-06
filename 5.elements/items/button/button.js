8849923801c2fa7ff5317a160772f8abd0dbc87c
Dejan Tomic <dejan@Dejans-Mac-mini.local>
2025-03-06 12:38:20 +0100

    A

--- 5.elements/items/button/button.js
+++ 5.elements/items/button/button.js
@@ -4,6 +4,14 @@ elements.OnReady(() =>
         id: 'dh-button',
         render: function ()
         {
+            this.Define({
+                name: () => {
+                    return {
+                        id: 0
+                    }
+                },
+            });
+
             return `
                 <dh-icon name="airplane"></dh-icon>
                 <button>{{name}}</button>
